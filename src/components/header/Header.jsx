7080154e5aa1ddd12fb12e8536759385ff3bcfe3
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-22 20:12:07 +0430

    Make responsive header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./Header.css";
+
+import { Container } from "reactstrap";
+
+const navLinks = [
+  {
+    display: "Home",
+    url: "#",
+  },
+  {
+    display: "About",
+    url: "#",
+  },
+
+  {
+    display: "Menu",
+    url: "#",
+  },
+  
+  {
+    display: "Contact",
+    url: "#",
+  },
+];
+
+const Header = () => {
+
+  return (
+    <header className="header">
+      <Container>
+        <div className="navigation">
+          <div className="logo">
+            <h2 className=" d-flex align-items-center gap-1">
+              <span>
+                <i class="ri-restaurant-2-line"></i>
+              </span>{" "}
+              Zhala
+            </h2>
+          </div>
+
+          <div className="nav__menu ">
+            <div className="nav__list__wrapper d-flex align-items-center gap-5">
+              <ul className="nav__list">
+                {navLinks.map((item, index) => (
+                  <li className="nav__item" key={index}>
+                    <a href={item.url}>
+                      {item.display}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+
+              <div className="menu__right">
+                <div className="custom__search ">
+                  <input type="text" placeholder="search item...." />
+                  <span>
+                    <i class="ri-search-line"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <span className="cart__icon">
+              <i class="ri-shopping-basket-line"></i>
+
+              <span className="badge">2</span>
+            </span>
+          </div>
+
+          <div className="mobile__menu">
+            <span>
+              <i class="ri-menu-line"></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import {React, Fragment} from "react";
+import Header from "../components/header/Header";
+
+const Home = () => {
+    return (
+        <Fragment>
+            <Header />
+        </Fragment>
+    )
+}
+
+export default Home;
