90e81d7e19611ddd4eab2ee61e79840560398cbb
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-23 12:19:26 +0430

    Make the mobile menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useRef} from "react";
 import "./Header.css";
 
 import { Container } from "reactstrap";
@@ -25,6 +25,9 @@ const navLinks = [
 ];
 
 const Header = () => {
+  const menuRef = useRef()
+
+  const menuToggle = () => menuRef.current.classList.toggle('active__menu')
 
   return (
     <header className="header">
@@ -34,17 +37,17 @@ const Header = () => {
             <h2 className=" d-flex align-items-center gap-1">
               <span>
                 <i class="ri-restaurant-2-line"></i>
-              </span>{" "}
+              </span>
               Zhala
             </h2>
           </div>
 
-          <div className="nav__menu ">
+          <div className="nav__menu" ref={menuRef}>
             <div className="nav__list__wrapper d-flex align-items-center gap-5">
               <ul className="nav__list">
                 {navLinks.map((item, index) => (
                   <li className="nav__item" key={index}>
-                    <a href={item.url}>
+                    <a href={item.url}  onClick={menuToggle} >
                       {item.display}
                     </a>
                   </li>
@@ -72,7 +75,7 @@ const Header = () => {
 
           <div className="mobile__menu">
             <span>
-              <i class="ri-menu-line"></i>
+              <i class="ri-menu-line" onClick={menuToggle}></i>
             </span>
           </div>
         </div>
