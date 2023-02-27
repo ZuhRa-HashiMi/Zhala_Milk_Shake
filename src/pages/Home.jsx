4ef6c7fabf44e6ed8026343c49bd4fd47f67cca5
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-28 00:22:55 +0430

    Make the slider part

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import {React, Fragment} from "react";
 import Header from "../components/header/Header";
+import HeroSlider from "../components/hero-slider/HeroSlider";
 
 const Home = () => {
     return (
         <Fragment>
             <Header />
+            <HeroSlider />
         </Fragment>
     )
 }
