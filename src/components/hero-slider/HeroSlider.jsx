011d802450be8324a5e7007a54a108c581eecee5
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-01 11:22:33 +0500

    Change the mobile view

--- src/components/hero-slider/HeroSlider.jsx
+++ src/components/hero-slider/HeroSlider.jsx
@@ -5,39 +5,38 @@ import Slider from "react-slick";
 import { sliderData } from "../../assests/face-data/slider";
 import "./slider.css";
 
-
 const HeroSlider = () => {
-    const settings = {
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 3000,
-    };
-    return (
-      <section>
-        <Container>
-          <Slider {...settings}>
-            {sliderData.map((item) => (
-              <div key={item.id}>
-                <div className="slider__wrapper d-flex align-items-center justify-content-between pt-5">
-                  <div className="slider__content w-50 ps-2">
-                    <h2 className="mb-3 ">{item.title}</h2>
-                    <p>{item.desc}</p>
-                    <button className="btn">Explore Food</button>
-                  </div>
-  
-                  <div className="slider__img w-50">
-                    <img src={item.imgUrl} alt="" className="w-100" />
-                  </div>
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+  return (
+    <section>
+      <Container>
+        <Slider {...settings}>
+          {sliderData.map((item) => (
+            <div key={item.id}>
+              <div className="slider__wrapper d-flex flex-column-reverse flex-lg-row flex-md-row flex-sm-column-reverse align-items-center justify-content-between pt-5">
+                <div className="slider__content w-100 px-4">
+                  <h2 className="mb-3">{item.title}</h2>
+                  <p className="text-justify">{item.desc}</p>
+                  <button className="btn">Explore Food</button>
+                </div>
+
+                <div className="slider__img w-100 px-4">
+                  <img src={item.imgUrl} alt="" className="w-100" />
                 </div>
               </div>
-            ))}
-          </Slider>
-        </Container>
-      </section>
-    );
-  };
-  
-  export default HeroSlider;
+            </div>
+          ))}
+        </Slider>
+      </Container>
+    </section>
+  );
+};
+
+export default HeroSlider;
